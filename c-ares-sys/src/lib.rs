385f823387ae7af8d5183adb9350dbc08b5e9bd2
David Hotham <david.hotham@blueyonder.co.uk>
2015-07-17 18:49:10 +0100

    Try to get autogenerated docs

--- c-ares-sys/src/lib.rs
+++ c-ares-sys/src/lib.rs
@@ -1,3 +1,8 @@
+//! Low-level bindings for the c-ares library.
+//!
+//! In most cases this crate should not be used directly.  The c-ares crate
+//! provides a safe wrapper and should be preferred wherever possible.
+
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 

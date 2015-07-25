16d539385229a03c68758124fba2e9cad5ecc8a9
David Hotham <david.hotham@blueyonder.co.uk>
2015-07-25 11:42:11 +0100

    Documentation

--- src/types.rs
+++ src/types.rs
@@ -15,30 +15,79 @@ pub const INVALID_FD: io::RawFd = c_ares_sys::ARES_SOCKET_BAD as io::RawFd;
 /// error code into a description.
 #[derive(Debug, Clone, Copy)]
 pub enum AresError {
+    /// DNS server returned answer with no data.
     ENODATA = c_ares_sys::ARES_ENODATA as isize,
+
+    /// DNS server claims query was misformatted.
     EFORMERR = c_ares_sys::ARES_EFORMERR  as isize,
+
+    /// DNS server returned general failure.
     ESERVFAIL = c_ares_sys::ARES_ESERVFAIL as isize,
+
+    /// Domain name not found.
     ENOTFOUND = c_ares_sys::ARES_ENOTFOUND as isize,
+
+    /// DNS server does not implement requested operation.
     ENOTIMP = c_ares_sys::ARES_ENOTIMP as isize,
+
+    /// DNS server refused query.
     EREFUSED = c_ares_sys::ARES_EREFUSED as isize,
+
+    /// Misformatted DNS query.
     EBADQUERY = c_ares_sys::ARES_EBADQUERY as isize,
+
+    /// Misformatted domain name.
     EBADNAME = c_ares_sys::ARES_EBADNAME as isize,
+
+    /// Unsupported address family.
     EBADFAMILY = c_ares_sys::ARES_EBADFAMILY as isize,
+
+    /// Misformatted DNS reply.
     EBADRESP = c_ares_sys::ARES_EBADRESP as isize,
+
+    /// Could not contact DNS servers.
     ECONNREFUSED = c_ares_sys::ARES_ECONNREFUSED as isize,
+
+    /// Timeout while contacting DNS servers.
     ETIMEOUT = c_ares_sys::ARES_ETIMEOUT as isize,
+
+    /// End of file.
     EOF = c_ares_sys::ARES_EOF as isize,
+
+    /// Error reading file.
     EFILE = c_ares_sys::ARES_EFILE as isize,
+
+    /// Out of memory.
     ENOMEM = c_ares_sys::ARES_ENOMEM as isize,
+
+    /// Channel is being destroyed.
     EDESTRUCTION = c_ares_sys::ARES_EDESTRUCTION as isize,
+
+    /// Misformatted string.
     EBADSTR = c_ares_sys::ARES_EBADSTR as isize,
+
+    /// Illegal flags specified.
     EBADFLAGS = c_ares_sys::ARES_EBADFLAGS as isize,
+
+    /// Given hostname is not numeric.
     ENONAME = c_ares_sys::ARES_ENONAME as isize,
+
+    /// Illegal hints flags specified.
     EBADHINTS = c_ares_sys::ARES_EBADHINTS as isize,
+
+    /// c-ares library initialization not yet performed.
     ENOTINITIALIZED = c_ares_sys::ARES_ENOTINITIALIZED as isize,
+
+    /// rror loading iphlpapi.dll.
     ELOADIPHLPAPI = c_ares_sys::ARES_ELOADIPHLPAPI as isize,
+
+    /// ould not find GetNetworkParams function.
     EADDRGETNETWORKPARAMS = c_ares_sys::ARES_EADDRGETNETWORKPARAMS as isize,
+
+    /// DNS query cancelled.
     ECANCELLED = c_ares_sys::ARES_ECANCELLED as isize,
+
+    /// Unknown error.
     UNKNOWN,
 }
 

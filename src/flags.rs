ed77b3eb0523e712f49dbdd4f8552374bd167490
David Hotham <david.hotham@blueyonder.co.uk>
2015-09-17 20:22:50 +0100

    Remove ugly rustdoc workaround - no longer needed

--- src/flags.rs
+++ src/flags.rs
@@ -1,48 +1,44 @@
 extern crate c_ares_sys;
 
 bitflags!(
-    #[doc = "Flags that may be passed when initializing a channel."]
+    /// Flags that may be passed when initializing a channel.
     flags Flags: i32 {
-        #[doc = "Always use TCP queries (the \"virtual circuit\") instead of"]
-        #[doc = "UDP queries.  Normally, TCP is only used if a UDP query"]
-        #[doc = "yields a truncated result."]
+        /// Always use TCP queries (the "virtual circuit") instead of UDP
+        /// queries.  Normally, TCP is only used if a UDP query yields a
+        /// truncated result.
         const USEVC = c_ares_sys::ARES_FLAG_USEVC,
 
-        #[doc = "Only query the first server in the list of servers to"]
-        #[doc = "query."]
+        /// Only query the first server in the list of servers to query.
         const PRIMARY = c_ares_sys::ARES_FLAG_PRIMARY,
 
-        #[doc = "If a truncated response to a UDP query is received, do not"]
-        #[doc = "fall back to TCP; simply continue on with the truncated"]
-        #[doc = "response."]
+        /// If a truncated response to a UDP query is received, do not fall
+        /// back to TCP; simply continue on with the truncated response.
         const IGNTC = c_ares_sys::ARES_FLAG_IGNTC,
 
-        #[doc = "Do not set the \"recursion desired\" bit on outgoing"]
-        #[doc = "queries, so that the name server being contacted will not"]
-        #[doc = "try to fetch the answer from other servers if it doesn't"]
-        #[doc = "know the answer locally."]
+        /// Do not set the "recursion desired" bit on outgoing queries, so
+        /// that the name server being contacted will not try to fetch the
+        /// answer from other servers if it doesn't know the answer locally.
         const NORECURSE = c_ares_sys::ARES_FLAG_NORECURSE,
 
-        #[doc = "Do not close communications sockets when the number of"]
-        #[doc = "active queries drops to zero."]
+        /// Do not close communications sockets when the number of active
+        /// queries drops to zero.
         const STAYOPEN = c_ares_sys::ARES_FLAG_STAYOPEN,
 
-        #[doc = "Do not use the default search domains; only query hostnames"]
-        #[doc = "as-is or as aliases."]
+        /// Do not use the default search domains; only query hostnames as-is
+        /// or as aliases.
         const NOSEARCH = c_ares_sys::ARES_FLAG_NOSEARCH,
 
-        #[doc = "Do not honor the HOSTALIASES environment variable, which"]
-        #[doc = "normally specifies a file of hostname translations."]
+        /// Do not honor the HOSTALIASES environment variable, which normally
+        /// specifies a file of hostname translations.
         const NOALIASES = c_ares_sys::ARES_FLAG_NOALIASES,
 
-        #[doc = "Do not discard responses with the SERVFAIL, NOTIMP, or"]
-        #[doc = "REFUSED response code or responses whose questions don't"]
-        #[doc = "match the questions in the request. Primarily useful for"]
-        #[doc = "writing clients which might be used to test or debug name"]
-        #[doc = "servers."]
+        /// Do not discard responses with the SERVFAIL, NOTIMP, or REFUSED
+        /// response code or responses whose questions don't match the
+        /// questions in the request. Primarily useful for writing clients
+        /// which might be used to test or debug name servers.
         const NOCHECKRESP = c_ares_sys::ARES_FLAG_NOCHECKRESP,
 
-        #[doc = "Use Extension Mechanisms for DNS."]
+        /// Use Extension Mechanisms for DNS.
         const EDNS = c_ares_sys::ARES_FLAG_EDNS,
     }
 );
--- src/ni_flags.rs
+++ src/ni_flags.rs
@@ -1,44 +1,39 @@
 extern crate c_ares_sys;
 
 bitflags!(
-    #[doc = "Flags that may be provided on a call to `get_name_info()`."]
+    /// Flags that may be provided on a call to `get_name_info()`.
     flags NIFlags: i32 {
-        #[doc = "Only the nodename portion of the FQDN is returned for local"]
-        #[doc = "hosts."]
+        /// Only the nodename portion of the FQDN is returned for local hosts.
         const NOFQDN = c_ares_sys::ARES_NI_NOFQDN,
 
-        #[doc = "The numeric form of the hostname is returned rather than the"]
-        #[doc = "name."]
+        /// The numeric form of the hostname is returned rather than the name.
         const NUMERICHOST = c_ares_sys::ARES_NI_NUMERICHOST,
 
-        #[doc = "An error is returned if the hostname cannot be found in the"]
-        #[doc = "DNS."]
+        /// An error is returned if the hostname cannot be found in the DNS.
         const NAMEREQD = c_ares_sys::ARES_NI_NAMEREQD,
 
-        #[doc = "The numeric form of the service is returned rather than the"]
-        #[doc = "name."]
+        /// The numeric form of the service is returned rather than the name.
         const NUMERICSERV = c_ares_sys::ARES_NI_NUMERICSERV,
 
-        #[doc = "The service name is to be looked up for the TCP protocol."]
+        /// The service name is to be looked up for the TCP protocol.
         const TCP = c_ares_sys::ARES_NI_TCP,
 
-        #[doc = "The service name is to be looked up for the UDP protocol."]
+        /// The service name is to be looked up for the UDP protocol.
         const UDP = c_ares_sys::ARES_NI_UDP,
 
-        #[doc = "The service name is to be looked up for the SCTP protocol."]
+        /// The service name is to be looked up for the SCTP protocol.
         const SCTP = c_ares_sys::ARES_NI_SCTP,
 
-        #[doc = "The service name is to be looked up for the DCCP protocol."]
+        /// The service name is to be looked up for the DCCP protocol.
         const DCCP = c_ares_sys::ARES_NI_DCCP,
 
-        #[doc = "The numeric form of the scope ID is returned rather than"]
-        #[doc = "the name."]
+        /// The numeric form of the scope ID is returned rather than the name.
         const NUMERICSCOPE = c_ares_sys::ARES_NI_NUMERICSCOPE,
 
-        #[doc = "A hostname lookup is being requested."]
+        /// A hostname lookup is being requested.
         const LOOKUPHOST = c_ares_sys::ARES_NI_LOOKUPHOST,
 
-        #[doc = "A service name lookup is being requested."]
+        /// A service name lookup is being requested.
         const LOOKUPSERVICE = c_ares_sys::ARES_NI_LOOKUPSERVICE,
     }
 );

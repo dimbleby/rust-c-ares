730d780c30793e690cc429ff300d0413df698738
David Hotham <david.hotham@metaswitch.com>
2015-11-04 20:46:56 +0000

    No epoll on OSX

--- examples/cares-examples.rs
+++ examples/cares-examples.rs
@@ -1,10 +1,20 @@
 #[cfg(unix)]
-fn main() {
-    mod unix;
+mod unix;
+
+#[cfg(any(target_os = "linux", target_os = "android"))]
+fn epoll_examples() {
     unix::cares_epoll::main();
     unix::cares_epoll_resolver::main();
+}
+
+#[cfg(not(any(target_os = "linux", target_os = "android")))]
+fn epoll_examples() { }
+
+#[cfg(unix)]
+fn main() {
     unix::cares_event_loop::main();
     unix::cares_event_loop_resolver::main();
+    epoll_examples();
 }
 
 #[cfg(windows)]
--- examples/unix/mod.rs
+++ examples/unix/mod.rs
@@ -1,4 +1,8 @@
-pub mod cares_epoll;
-pub mod cares_epoll_resolver;
 pub mod cares_event_loop;
 pub mod cares_event_loop_resolver;
+
+#[cfg(any(target_os = "linux", target_os = "android"))]
+pub mod cares_epoll;
+
+#[cfg(any(target_os = "linux", target_os = "android"))]
+pub mod cares_epoll_resolver;

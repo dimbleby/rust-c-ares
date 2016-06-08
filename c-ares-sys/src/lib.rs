ec2204619588ec984db7e639402979e6153bb49d
David Hotham <david.hotham@metaswitch.com>
2016-06-08 15:13:49 +0100

    Remove unnecessary annotations
    Now included in code autogenerated by bindgen

--- c-ares-sys/src/lib.rs
+++ c-ares-sys/src/lib.rs
@@ -3,8 +3,6 @@
 //! In most cases this crate should not be used directly.  The c-ares crate
 //! provides a safe wrapper and should be preferred wherever possible.
 
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
 extern crate libc;
 extern crate c_types;
 

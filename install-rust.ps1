e6d14e470e448d11838a38eaabc8684ffd8f9686
David Hotham <david.hotham@blueyonder.co.uk>
2016-09-24 15:28:26 +0100

    appveyor workaround

--- install-rust.ps1
+++ install-rust.ps1
@@ -3,12 +3,12 @@ param([string]$channel=${env:channel}, [string]$target=${env:target})
 $downloadUrl = "https://static.rust-lang.org/dist/"
 $manifest = "channel-rust-${channel}"
 $localManifest = "${env:Temp}\${manifest}"
-Start-FileDownload "${downloadUrl}${manifest}" -FileName "${localManifest}"
+appveyor DownloadFile "${downloadUrl}${manifest}" -FileName "${localManifest}"
 
 $match = Get-Content "${localManifest}" | Select-String -pattern "${target}.exe" -simplematch
 $installer = $match.line
 $localInstaller = "${env:Temp}\${installer}"
-Start-FileDownload "${downloadUrl}${installer}" -FileName "${localInstaller}"
+appveyor DownloadFile "${downloadUrl}${installer}" -FileName "${localInstaller}"
 
 $installDir = "C:\Rust"
 &"${localInstaller}" /VERYSILENT /NORESTART /DIR="${installDir}" | Write-Output

0c81d9b710b09322abf2d3a2444413d6347e5f05
David Hotham <david.hotham@metaswitch.com>
2016-10-06 19:24:52 +0100

    Revert "appveyor workaround"
    This reverts commit e6d14e470e448d11838a38eaabc8684ffd8f9686.

--- install-rust.ps1
+++ install-rust.ps1
@@ -3,12 +3,12 @@ param([string]$channel=${env:channel}, [string]$target=${env:target})
 $downloadUrl = "https://static.rust-lang.org/dist/"
 $manifest = "channel-rust-${channel}"
 $localManifest = "${env:Temp}\${manifest}"
-appveyor DownloadFile "${downloadUrl}${manifest}" -FileName "${localManifest}"
+Start-FileDownload "${downloadUrl}${manifest}" -FileName "${localManifest}"
 
 $match = Get-Content "${localManifest}" | Select-String -pattern "${target}.exe" -simplematch
 $installer = $match.line
 $localInstaller = "${env:Temp}\${installer}"
-appveyor DownloadFile "${downloadUrl}${installer}" -FileName "${localInstaller}"
+Start-FileDownload "${downloadUrl}${installer}" -FileName "${localInstaller}"
 
 $installDir = "C:\Rust"
 &"${localInstaller}" /VERYSILENT /NORESTART /DIR="${installDir}" | Write-Output

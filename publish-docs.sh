9ce8399705d3aae11cb8aad9bb2f6aa33525c7f0
David Hotham <david.hotham@metaswitch.com>
2016-10-18 22:47:39 +0100

    Only generate docs for publishing

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+cargo doc -p c-ares
+echo "<meta http-equiv=refresh content=0;url=c_ares/index.html>" > target/doc/index.html
+
+git config user.name "Travis CI"
+~/.local/bin/ghp-import -n target/doc
+
 openssl aes-256-cbc -K "$encrypted_eab4824118db_key" -iv "$encrypted_eab4824118db_iv" -in publish-key.enc -out ~/.ssh/publish-key -d
 chmod u=rw,og= ~/.ssh/publish-key
 echo "Host github.com" >> ~/.ssh/config
 echo "  IdentityFile ~/.ssh/publish-key" >> ~/.ssh/config
 git remote set-url origin git@github.com:dimbleby/rust-c-ares.git
-git config user.name "Travis CI"
-echo "<meta http-equiv=refresh content=0;url=c_ares/index.html>" > target/doc/index.html
-~/.local/bin/ghp-import -n target/doc
 git push origin +gh-pages

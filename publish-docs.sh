abf9aaa4390f7ec09d31d8e02b1806d601f061b8
David Hotham <david.hotham@metaswitch.com>
2016-11-20 17:42:53 +0000

    only install ghp-import when needed

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -e
 
 cargo doc -p c-ares
 echo "<meta http-equiv=refresh content=0;url=c_ares/index.html>" > target/doc/index.html
 
 git config user.name "Travis CI"
+pip install --user ghp-import
 ~/.local/bin/ghp-import -n target/doc
 
 openssl aes-256-cbc -K "$encrypted_eab4824118db_key" -iv "$encrypted_eab4824118db_iv" -in publish-key.enc -out ~/.ssh/publish-key -d

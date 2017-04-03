8f77e8a1e69c011aeadf2ec0efa0db4475a7038d
David Hotham <david.hotham@blueyonder.co.uk>
2017-04-03 23:12:58 +0100

    prepare for servo/rust-bindgen#603 to be fixed

--- c-ares-sys/generate-ffi.sh
+++ c-ares-sys/generate-ffi.sh
@@ -6,14 +6,16 @@ then
   exit 1
 fi
 
-# Prepare for bindgen, do it, and then apply manual patches.
+# Prepare for bindgen, and do it.
 (cd c-ares && ./buildconf && ./configure)
 bindgen --whitelist-function="ares.*" --whitelist-type="ares.*" --whitelist-type="apattern" --no-recursive-whitelist --no-unstable-rust --output=src/ffi.rs c-ares/ares.h
-patch -p0 < ffi.patch
 
 # bindgen converts 'char' to 'c_schar' - see https://github.com/servo/rust-bindgen/issues/603.
 # Since c-ares never uses 'signed char' we can compensate with simple search-and-replace.
 sed -i 's/c_schar/c_char/g' src/ffi.rs
+
+# Apply manual patches.
+patch -p0 < ffi.patch
 
 # Generate constants.
 ./generate-constants.pl > src/constants.rs

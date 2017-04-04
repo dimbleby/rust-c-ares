6730d752b19b4f887fa7d844b2c9244a12fb9715
David Hotham <david.hotham@metaswitch.com>
2017-04-04 11:51:10 +0100

    Remove workaround for bindgen bug

--- c-ares-sys/generate-ffi.sh
+++ c-ares-sys/generate-ffi.sh
@@ -10,10 +10,6 @@ fi
 (cd c-ares && ./buildconf && ./configure)
 bindgen --whitelist-function="ares.*" --whitelist-type="ares.*" --whitelist-type="apattern" --no-recursive-whitelist --no-unstable-rust --output=src/ffi.rs c-ares/ares.h
 
-# bindgen converts 'char' to 'c_schar' - see https://github.com/servo/rust-bindgen/issues/603.
-# Since c-ares never uses 'signed char' we can compensate with simple search-and-replace.
-sed -i 's/c_schar/c_char/g' src/ffi.rs
-
 # Apply manual patches.
 patch -p0 < ffi.patch
 

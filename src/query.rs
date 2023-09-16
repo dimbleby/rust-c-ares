f473c9899f9cb9bea66c7694e21150b0eaf745bd
David Hotham <david.hotham@blueyonder.co.uk>
2023-09-16 15:43:55 +0100

    reduce scope of panic handling
    the code in this library does not panic, it's only the user-provided
    callbacks that might

--- src/query.rs
+++ src/query.rs
@@ -13,14 +13,12 @@ pub(crate) unsafe extern "C" fn query_callback<F>(
 ) where
     F: FnOnce(Result<&[u8]>) + Send + 'static,
 {
-    panic::catch(|| {
-        let result = if status == c_ares_sys::ARES_SUCCESS {
-            let data = slice::from_raw_parts(abuf, alen as usize);
-            Ok(data)
-        } else {
-            Err(Error::from(status))
-        };
-        let handler = Box::from_raw(arg as *mut F);
-        handler(result);
-    });
+    let result = if status == c_ares_sys::ARES_SUCCESS {
+        let data = slice::from_raw_parts(abuf, alen as usize);
+        Ok(data)
+    } else {
+        Err(Error::from(status))
+    };
+    let handler = Box::from_raw(arg as *mut F);
+    panic::catch(|| handler(result));
 }

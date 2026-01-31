24e69b007d6d4d2eca15b93ad8e440dadcd7554a
David Hotham <david.hotham@blueyonder.co.uk>
2026-01-31 21:28:03 +0000

    Avoid calling a str static when it is not
    I think no soundness issue here because the lie did not escape the
    implementation.

--- src/string.rs
+++ src/string.rs
@@ -7,24 +7,19 @@ use crate::utils::c_string_as_str_unchecked;
 /// A smart pointer wrapping a string as allocated by c-ares.
 pub struct AresString {
     ares_string: *mut c_char,
-    rust_str: &'static str,
 }
 
 impl AresString {
     #[allow(dead_code)]
     pub(crate) fn new(ares_string: *mut c_char) -> Self {
-        let rust_str = unsafe { c_string_as_str_unchecked(ares_string) };
-        AresString {
-            ares_string,
-            rust_str,
-        }
+        AresString { ares_string }
     }
 }
 
 impl Deref for AresString {
     type Target = str;
     fn deref(&self) -> &Self::Target {
-        self.rust_str
+        unsafe { c_string_as_str_unchecked(self.ares_string) }
     }
 }
 
